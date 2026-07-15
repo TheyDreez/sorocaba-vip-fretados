682eddbac3aa18fff3eb6594046581b345fa9be8
TheyDreez <109829846+TheyDreez@users.noreply.github.com>
2026-07-14 21:25:05 -0300

    chore: remove pricing and interior gallery modules per client request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Hero } from '@/components/Hero';
 import { TrustIndicators } from '@/components/TrustIndicators';
-import { PriceComparison } from '@/components/PriceComparison';
 import { History } from '@/components/History';
 import { Experience } from '@/components/Experience';
 import { SmartRouteAssistant } from '@/components/SmartRouteAssistant';
 import { RouteMap } from '@/components/RouteMap';
-import { Gallery } from '@/components/Gallery';
 import { FAQ } from '@/components/FAQ';
 import { ContactForm } from '@/components/ContactForm';
 import { FloatingWhatsApp } from '@/components/FloatingWhatsApp';
@@ -16,12 +14,10 @@ export default function Home() {
     <main>
       <Hero />
       <TrustIndicators />
-      <PriceComparison />
       <History />
       <Experience />
       <SmartRouteAssistant />
       <RouteMap />
-      <Gallery />
       <FAQ />
       <ContactForm />
       <Footer />
